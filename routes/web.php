2025cd8252a66972cb3bbb0f69cee39e093eada4
manalap <manuchian@yahoo.com>
2021-04-18 16:02:18 +0300

    fake database seeding

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Models\Link::all();
+    return view('welcome',['links' => $links]);
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
